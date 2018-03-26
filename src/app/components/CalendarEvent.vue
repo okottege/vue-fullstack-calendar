9d4da27d63533e8eb50dae48a4362eae16c3be38
Oshan Kottege <oshan.kottege@gmail.com>
2018-03-26 22:09:17 +1300

    Added all of the static components and got everything working.

---
+++ src/app/components/CalendarEvent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="day-event" :style="getEventBackgroundColour()">
+    <div>
+      <span class="has-text-centered details">{{ event.details }}</span>
+      <div class="has-text-centered icons">
+        <i class="fa fa-pencil-square edit-icon"></i>
+        <i class="fa fa-trash-o delete-icon"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalendarEvent',
+  props: ['event', 'day'],
+  computed: {
+    getEventBackgroundColour() {
+      const colours = ['#FF9999', '#85D6FF', '#99FF99'];
+      let randomColour = colours[Math.floor(Math.random() * colours.length)];
+      return `background-color: ${randomColour}`;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.day-event {
+  margin-top: 6px;
+  margin-bottom: 6px;
+  display: block;
+  color: #4C4C4C;
+  padding: 5px;
+
+  .details {
+    display: block;
+  }
+
+  input {
+    background: none;
+    border: 0;
+    border-bottom: 1px solid #FFF;
+    width: 100%;
+
+    &:focus {
+      outline: none;
+    }
+  }
+}
+</style>
+
---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import { seedData } from './seed';
+
+export const store = {
+  state: {
+    seedData
+  }
+};
