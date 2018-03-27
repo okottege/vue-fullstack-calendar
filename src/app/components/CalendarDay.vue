52bf28afffb95499d70e645a00e8adb85c1bad63
Oshan Kottege <oshan.kottege@gmail.com>
2018-03-27 22:01:54 +1300

    Added interaction to the calendar application.

--- src/app/components/CalendarDay.vue
+++ src/app/components/CalendarDay.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="day column">
+  <div class="day column" @click="setActiveDay(day.id)">
     <div class="day-banner has-text-centered">{{ day.abbvTitle }}</div>
       <div class="day-details">
         <div class="day-number">{{ day.id }}</div>
@@ -13,10 +13,16 @@
 
 <script>
 import CalendarEvent from './CalendarEvent';
+import { store } from '../store';
 
 export default {
   name: 'CalendarDay',
   props: ['day'],
+  methods: {
+    setActiveDay(dayId) {
+      store.setActiveDay(dayId);
+    }
+  },
   components: {
     CalendarEvent
   }
--- src/app/store.js
+++ src/app/store.js
@@ -4,5 +4,17 @@ import { seedData } from './seed';
 export const store = {
   state: {
     seedData
+  },
+  getActiveDay() {
+    return this.state.seedData.find(day => day.active);
+  },
+  setActiveDay(dayId) {
+    this.state.seedData.map(day => {
+      day.active = day.id === dayId;
+    });
+  },
+  submitEvent(eventDetails) {
+    const activeDay = this.getActiveDay();
+    activeDay.events.push({ 'details': eventDetails, 'edit': false });
   }
 };
